e3148c4e0815e2eaaa5c660e71e9df50cffdcf67
Gadiel Sosa <sosachanantonio0@gmail.com>
2025-10-16 10:56:01 -0600

    feat(login): add success/fail triggers with email and password regex validation

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -29,6 +29,59 @@ class _LoginScreenState extends State<LoginScreen> {
   final passwordFocus = FocusNode();
   // 3.2 Crear timer para detener la animación al dejar de teclear email
   Timer? _typingDebouncer;
+  //Paso 4.1 Declarar controllers
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  //4.2 Errores para mostrar en la UI
+  String? emailError;
+  String? passwordError;
+  //4.3 Validadores
+  bool isValidEmail(String email) {
+    // Expresiones regulares
+    final emailRegex = RegExp(r'^[^\s@]+@[^\s@]+\.[^\s@]+$');
+    return emailRegex.hasMatch(email);
+  }
+
+  bool isValidPassword(String password) {
+    // mínimo 8, una mayúscula, una minúscula, un dígito y un especial
+    final passwordRegex = RegExp(
+      r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}$',
+    );
+    return passwordRegex.hasMatch(password);
+  }
+
+  // 4.4 accion al boton login
+  void _onlogin() {
+    final email = emailController.text.trim();
+    final password = passwordController.text;
+
+    // Recalcular errores
+    final eError = isValidEmail(email) ? null : 'Email no válido';
+    final pError =
+        isValidPassword(password)
+            ? null
+            : 'Mínimo 8 caracteres, una mayúscula, una minúscula, un número y un especial';
+    // para avisar que hubo un cambio
+    setState(() {
+      emailError = eError;
+      passwordError = pError;
+    });
+    // 4.5 Cerrar el teclado y bajas
+    FocusScope.of(context).unfocus();
+    _typingDebouncer?.cancel();
+    isCheking?.change(false);
+    isHandsUp?.change(false);
+    numLook?.value = 50.0; // Mirada neutral
+
+    // 4.7 Activar Triggers
+    if (eError == null && pError == null) {
+      trigSuccess?.fire();
+    } else {
+      trigFail?.fire();
+    }
+
+  }
+
   // Listeners oyentes
 
   @override
@@ -88,8 +141,10 @@ class _LoginScreenState extends State<LoginScreen> {
               TextField(
                 // llamado a los oyentes
                 focusNode: emailFocus,
+                // 4.8 enlazar controldores al texfield
+                controller: emailController,
                 onChanged: (value) {
-                  if (isHandsUp != null) {
+
                     // estoy escribiendo
                     isCheking!.change(true);
                     // ajuste de limite 0 a 100
@@ -102,12 +157,12 @@ class _LoginScreenState extends State<LoginScreen> {
                     _typingDebouncer = Timer(const Duration(seconds: 4), () {
                       if (!mounted) {
                         return; // si la pantalla se cierra
-                      } 
+                      }
 
                       // Mirada neutral al dejar de teclear email
                       isCheking?.change(false);
                     });
-                  }
+                  
 
                   if (isCheking == null) return;
                   // Activa el modo chismoso
@@ -118,6 +173,8 @@ class _LoginScreenState extends State<LoginScreen> {
 
                 keyboardType: TextInputType.emailAddress,
                 decoration: InputDecoration(
+                  //4.9 mostrar el texto de error
+                  errorText: emailError,
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(12),
                   ),
@@ -130,6 +187,7 @@ class _LoginScreenState extends State<LoginScreen> {
               // Campo de texto password
               TextField(
                 focusNode: passwordFocus,
+                controller: passwordController,
                 onChanged: (value) {
                   if (isCheking != null) {
                     // No tapar los ojos al escribir mail
@@ -139,9 +197,10 @@ class _LoginScreenState extends State<LoginScreen> {
                   // Activa el modo chismoso
                   isHandsUp!.change(true);
                 },
-                controller: _userPasswordController,
+                //controller: _userPasswordController,
                 obscureText: !_passwordVisible,
                 decoration: InputDecoration(
+                  errorText: passwordError,
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(12),
                   ),
@@ -183,9 +242,7 @@ class _LoginScreenState extends State<LoginScreen> {
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(12),
                 ),
-                onPressed: () {
-                  //TO DO:
-                },
+                onPressed: _onlogin,
                 child: Text('Login', style: TextStyle(color: Colors.white)),
               ),
               const SizedBox(height: 10),
@@ -218,6 +275,9 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   void dispose() {
+    // 4.11 limpiar controladores
+    emailController.dispose();
+    passwordController.dispose();
     _userPasswordController.dispose();
     emailFocus.dispose();
     passwordFocus.dispose();
